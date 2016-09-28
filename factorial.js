03bf934fcc42fe9ba4f369e8a3e33e9b68bb6f95
Mauricio Orozco <Mauricio Orozco>
2016-09-28 16:42:38 -0400

    Updated altered files

---
+++ factorial.js
@@ -0,0 +1,8 @@
+var i = 1;
+var result = 1;
+
+  while (i <= 13) {
+  	result = i * result;
+  	i++;
+  	console.log (i);
+}
---
+++ odds.js
@@ -0,0 +1,9 @@
+for (var i = -50; i <=50; i++) {
+	if (i === 0) {
+		console.log(i);
+	} else if (i % 2 === 0) {
+		console.log('Even: ' + i);
+	} else {
+		console.log('Odd:  ' + i);
+	}
+} 
